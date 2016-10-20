0c564a887be2e4e41cb20adde5f89b08f02762f1
Nik Martelaro <nmartelaro@gmail.com>
2016-10-20 10:08:36 -0700

    splitting out overlaps in map step. starter code

--- A2/driver_stats_map.py
+++ A2/driver_stats_map.py
@@ -8,10 +8,17 @@ This file is saved as driver_stats_map.py with execute permission
 (chmod +x driver_stats_map.py)"""
 
 import sys
+from datetime import datetime
 
 # CONSTANTS
 HACK_IDX = 1
 PICKUP_TIME_IDX = 5
+DROPOFF_TIME_IDX = 6
+TRIP_DIST_IDX = 9
+FARE_IDX = 20
+
+FORMAT = "%Y-%m-%d %H:%M:%S"
+DATE = "%Y-%m-%d"
 
 def main():
     """Take lines from stdin and emit a key:value pair where
@@ -25,18 +32,45 @@ def main():
     """
     for line in sys.stdin:
         line_data = line.strip().split('\t')
-        pickup_time = line_data[PICKUP_TIME_IDX].split(" ") # split on the space to seperate the date and time
-        day = pickup_time[0]
-        hour = pickup_time[1][0:2] # hour is the first two numbers i.e. 12:00:00
+        pickup = line_data[PICKUP_TIME_IDX]
+        dropoff = line_data[DROPOFF_TIME_IDX]
+        p_time = datetime.strptime(pickup, FORMAT)
+        d_time = datetime.strptime(dropoff, FORMAT)
+        speed = float(line_data[TRIP_DIST_IDX]) / (d_time - p_time).total_seconds()
+        earnings_rate = float(line_data[FARE_IDX]) / (d_time - p_time).total_seconds()
+        print(earnings_rate*(d_time - p_time).total_seconds())
 
-        key = ",".join([line_data[HACK_IDX], day, hour])
-        value = ",".join(line_data)
-
-        # dont add the header
-        if "hack_license" in key:
-            pass
+        # find the hour borders (more than is up to 24:00, less than if across 24:00)
+        if d_time.hour > p_time.hour or d_time.hour < p_time.hour:
+            # spanning over 2 hour blocks
+            if d_time.hour > p_time.hour + 1 and float(line_data[FARE_IDX]) < 20:
+                # this doesn't really make sense for such a long ride
+                pass
+                #print(line_data)
+                #print(line_data[FARE_IDX])
         else:
-            print(key + "\t" + value)
+            pass
+            # key = ",".join([line_data[HACK_IDX], datetime.strftime(p_time, DATE), str(p_time.hour)])
+            # value = ",".join(line_data)
+            # print(key + "\t" + value)
+
+        # pickup_time = pickup.split(" ") # split on the space to seperate the date and time
+        # dropoff_time = dropoff.split(" ")
+        # pick_day = pickup_time[0]
+        # pick_hour = pickup_time[1][0:2] # hour is the first two numbers i.e. 12:00:00
+        #
+        #
+        # # find the drives that go over an hour boundary and split them into 2 lines
+        #
+        #
+        # key = ",".join([line_data[HACK_IDX], day, hour])
+        # value = ",".join(line_data)
+        #
+        # # dont add the header
+        # if "hack_license" in key:
+        #     pass
+        # else:
+        #     print(key + "\t" + value)
 
 if __name__ == "__main__":
     main()
